1cfbfd4698116d369826e3cf6d011e711cd27170
PhMemmel <65113153+PhMemmel@users.noreply.github.com>
2022-04-18 14:18:28 +0200

    Bump up version, restrict compatible moodle versions (#23)
    Co-authored-by: Philipp Memmel <philipp.memmel@gitlab.mebis.alp.dillingen.de>

--- version.php
+++ version.php
@@ -24,8 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2022031500;
+$plugin->version = 2022041700;
 $plugin->requires  = 2020061500;
+$plugin->supported = [39, 311];
+$plugin->incompatible = 400;
 $plugin->component = 'block_massaction';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v6.0.0';
+$plugin->release = 'v6.0.1';
